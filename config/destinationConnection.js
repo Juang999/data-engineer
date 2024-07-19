6c62f888c8fd05e503296328c5067483d8331ead
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-07-19 13:47:39 +0700

    update connection

--- config/destinationConnection.js
+++ config/destinationConnection.js
@@ -1,31 +1,31 @@
 const path = require('path');
 
-let envDevelopment = require('dotenv').config({path: path.resolve( './.env.development')});
-let envProduction = require('dotenv').config({path: path.resolve( './.env.production')});
+let {parsed: envDevelopment} = require('dotenv').config({path: path.resolve( './.env.development')});
+let {parsed: envProduction} = require('dotenv').config({path: path.resolve( './.env.production')});
 
 module.exports = {
     development: {
-        username: envDevelopment.parsed.DB_DESTINATION_USERNAME,
-        password: envDevelopment.parsed.DB_DESTINATION_PASSWORD,
-        database: envDevelopment.parsed.DB_DESTINATION_DATABASE,
-        host: envDevelopment.parsed.DB_DESTINATION_HOST,
-        port: envDevelopment.parsed.DB_DESTINATION_PORT,
-        dialect: envDevelopment.parsed.DB_DIALECT,
+        username: envDevelopment.DB_DESTINATION_USERNAME,
+        password: envDevelopment.DB_DESTINATION_PASSWORD,
+        database: envDevelopment.DB_DESTINATION_DATABASE,
+        host: envDevelopment.DB_DESTINATION_HOST,
+        port: envDevelopment.DB_DESTINATION_PORT,
+        dialect: envDevelopment.DB_DIALECT,
         dialectOptions: {
             useUTC: false
         },
-        timezone: envDevelopment.parsed.DB_TIMEZONE
+        timezone: envDevelopment.DB_TIMEZONE
     },
     production: {
-        username: envProduction.parsed.DB_DESTINATION_USERNAME,
-        password: envProduction.parsed.DB_DESTINATION_PASSWORD,
-        database: envProduction.parsed.DB_DESTINATION_DATABASE,
-        host: envProduction.parsed.DB_DESTINATION_HOST,
-        port: envProduction.parsed.DB_DESTINATION_PORT,
-        dialect: envProduction.parsed.DB_DIALECT,
+        username: envProduction.DB_DESTINATION_USERNAME,
+        password: envProduction.DB_DESTINATION_PASSWORD,
+        database: envProduction.DB_DESTINATION_DATABASE,
+        host: envProduction.DB_DESTINATION_HOST,
+        port: envProduction.DB_DESTINATION_PORT,
+        dialect: envProduction.DB_DIALECT,
         dialectOptions: {
             useUTC: false
         },
-        timezone: envProduction.parsed.DB_TIMEZONE
+        timezone: envProduction.DB_TIMEZONE
     }
 }
